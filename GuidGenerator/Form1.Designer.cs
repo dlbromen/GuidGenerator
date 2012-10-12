9e0d674b1e7db86d6cf34e1f604b7407c9a6b532
Derek Bromenshenkel <derek.bromenshenkel@gmail.com>
2012-10-12 17:44:35 -0500

    Initial commit

---
+++ GuidGenerator/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GuidGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDisplayGuids = new System.Windows.Forms.TextBox();
+            this.txtNumGuids = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDisplayGuids
+            // 
+            this.txtDisplayGuids.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDisplayGuids.BackColor = System.Drawing.SystemColors.Window;
+            this.txtDisplayGuids.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplayGuids.Location = new System.Drawing.Point(12, 41);
+            this.txtDisplayGuids.Multiline = true;
+            this.txtDisplayGuids.Name = "txtDisplayGuids";
+            this.txtDisplayGuids.ReadOnly = true;
+            this.txtDisplayGuids.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDisplayGuids.Size = new System.Drawing.Size(293, 88);
+            this.txtDisplayGuids.TabIndex = 0;
+            // 
+            // txtNumGuids
+            // 
+            this.txtNumGuids.Location = new System.Drawing.Point(166, 12);
+            this.txtNumGuids.Name = "txtNumGuids";
+            this.txtNumGuids.Size = new System.Drawing.Size(50, 20);
+            this.txtNumGuids.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(241, 10);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(64, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of GUIDs to generate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 141);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtNumGuids);
+            this.Controls.Add(this.txtDisplayGuids);
+            this.MinimumSize = new System.Drawing.Size(304, 150);
+            this.Name = "Form1";
+            this.Text = "GUID Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplayGuids;
+        private System.Windows.Forms.TextBox txtNumGuids;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GuidGenerator/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GuidGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateGuids(1);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            txtDisplayGuids.Text = string.Empty;
+            int guids = 1;
+
+            if (!string.IsNullOrEmpty(txtNumGuids.Text.Trim()))
+            {
+                try
+                {
+                    guids = Convert.ToInt32(txtNumGuids.Text.Trim());
+                }
+                catch
+                {
+                    txtDisplayGuids.Text = "Enter a NUMBER, not a letter, stupid. :)";
+                }
+            }
+
+            GenerateGuids(guids);
+        }
+
+        private void GenerateGuids(int guids)
+        {
+            if (guids > 50 && guids < 1000)
+            {
+                txtDisplayGuids.Text += "Are you sure??  Well, here goes.." + Environment.NewLine;
+            }
+            else if (guids >= 1000)
+            {
+                txtDisplayGuids.Text += "Oh, hellz no!" + Environment.NewLine;
+            }
+            else if (guids > 0)
+            {
+                if (guids == 1)
+                {
+                    Guid g = Guid.NewGuid();
+                    Clipboard.SetText(g.ToString().ToUpper());
+                    txtDisplayGuids.Text = g.ToString().ToUpper() + Environment.NewLine;
+                    txtDisplayGuids.Text += "(Automatically copied to clipboard)";
+                }
+                else
+                {
+                    for (int i = 0; i < guids; i++)
+                    {
+                        txtDisplayGuids.Text += Guid.NewGuid().ToString().ToUpper() + Environment.NewLine;
+                    }
+                }
+            }
+        }
+    }
+}
